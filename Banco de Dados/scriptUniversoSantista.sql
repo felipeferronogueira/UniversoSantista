5ae1601d39526102821c6e8f7b87cca4104246fe
felipeferronogueira <felipe.nogueira@sptech.school>
2025-05-01 13:35:17 -0300

    Fazendo login/cadastro

---
+++ Banco de Dados/scriptUniversoSantista.sql
@@ -0,0 +1,12 @@
+create database universosantista;
+
+use universosantista;
+
+create table usuario(
+idUsuario int primary key auto_increment,
+nome varchar(50),
+email varchar(50),
+senha varchar(50)
+);
+
+select * from usuario;
